53d114743676e7cf10a7f67c4c044556fb297427
AmitM0287 <amitmanna0287@gmail.com>
2021-08-13 12:11:05 +0530

    feat: mat-icon-module added

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatButtonModule } from '@angular/material/button';
-import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatIconModule } from '@angular/material/icon';
 
 const MaterialComponents = [
   MatCardModule,
   MatSnackBarModule,
   MatButtonModule,
-  FlexLayoutModule,
+  MatIconModule
 ]
 
 @NgModule({
